ab361447a626f57a5203b31a6e56851f8c2f9bb3
Heloisa <heloisa.b2006@aluno.ifsc.edu.br>
2026-06-28 23:25:02 -0300

    Ajuste layout visual

--- resources/views/gerenciador/central.blade.php
+++ resources/views/gerenciador/central.blade.php
@@ -11,7 +11,7 @@
             <div class="col-md-4 mb-3">
                 <div class="card shadow-sm h-100 border-primary">
                     <div class="card-body text-center">
-                        <h5 class="card-title text-primary">Cardápio</h5>
+                        <h5 class="card-title text-primary">CARDÁPIO</h5>
                         <p class="card-text">Veja o cardápio completo.</p>
                         <a href="{{ route('produtos.index') }}" class="btn btn-primary">Acessar</a>
                     </div>
